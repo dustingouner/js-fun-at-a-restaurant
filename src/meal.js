e64256dbe3667223c253bd5c979ac70cb6ab0fa1
Dustin Gouner <dustingouner20@gmail.com>
2023-02-01 12:23:16 -0700

    Complete Meal and pass all test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
+function nameMenuItem(itemName) {
+  var menuItemName = {
+    name: `Delicious ${itemName}`
+  }
+  return menuItemName.name
 
+}
+
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price, 
+    type: type
+  }
+  return menuItem
+}
+
+function addIngredients(ingredient, array) {
+  if (!array.includes(ingredient))
+  return array.push(ingredient)
+}
+
+function formatPrice(initialPrice) {
+  return '$' + initialPrice
+}
+
+function decreasePrice(price) {
+  return price * .90
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {title: title, ingredients: ingredients, type: menuItemType}
+  return recipe
 }
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
