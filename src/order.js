f24f63fd25b6cd7e64a499648da49a7c325d93fe
Dustin Gouner <dustingouner20@gmail.com>
2023-02-01 17:10:14 -0700

    Complete Order and pass all test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,51 @@
 
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3 ) {
+    return deliveryOrders.push(order)
+  } else
+    return order
+}
+
+function refundOrder(orderNum, deliveryOrders) {
+  for (var i = 0; i < deliveryOrders.length; i++) 
+    if (orderNum === deliveryOrders[i].orderNumber) {
+      return deliveryOrders.splice(i, 1)
+    }
+  }
+
+  function listItems(deliveryOrders) {
+    var item = []
+    for (var i = 0; i < deliveryOrders.length; i++) 
+      item.push(deliveryOrders[i].item)
+      return item.join(', ')
+  }
+
+  function searchOrder(deliveryOrders, itemName) {
+    for(var i = 0; i < deliveryOrders.length; i++) 
+      if(deliveryOrders[i].item === itemName) {
+        return true
+      } 
+      return false
+    }
+
+    
+
+
+
+
+
+  
+
+
+
+
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+
+
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
