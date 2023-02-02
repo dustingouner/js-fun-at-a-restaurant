fe2c69b66c58b980d0036200342b358d576b4d39
Dustin Gouner <dustingouner20@gmail.com>
2023-02-01 23:14:07 -0700

    Complete chef and pass all test

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,20 @@
 class Chef {
-  constructor() {
-    
+  constructor(chefName, restaurant) {
+    this.name = chefName
+    this.restaurant = restaurant
+  }
+  greetCustomer(customerName, time) {
+    if (time === true) {
+      return `Good morning, ${customerName}!`
+    } else
+      return `Hello, ${customerName}!`
+  }
+  checkForFood(foodItem) {
+    if(this.restaurant.menus[foodItem.type].includes(foodItem)) {
+      return `Yes, we're serving ${foodItem.name} today!`
+    } else {
+      return `Sorry, we aren't serving ${foodItem.name} today.`
+    }
   }
 }
 
